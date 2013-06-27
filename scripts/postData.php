0fa1b18a96a83cbff06a465c46e7958d4a23dd4e
ThermoMan <thestalwart1-tstat@yahoo.com>
2013-06-27 08:27:19 -0500

    Updated
    This might be a poor update.  The last firmware update to my TED5000
    borked the posting of usage data.  Before I tracked the problem down to
    the TED I had ripped this code open a couple of times.
    When I get a fix from the folks at http://www.theenergydetective.com/ I
    will have another update to this file

--- scripts/postData.php
+++ scripts/postData.php
@@ -1,42 +1,100 @@
 <?php
-require "../common.php";
+require dirname(__FILE__).'/../common.php';
+
 // Look here
 // http://www.theenergydetectiveforums.com/index.php/topic,4.0.html
 
-$text_string = $HTTP_RAW_POST_DATA;
+//$rootDir = '~/elec/';
+//$logDir =  $rootDir . 'logs/';
+//$logFile = $logDir . 'elec.' . date( 'Y.m.d' ) . '.log';
+$fp = fopen( 'testfile2', 'a' );
+//$fp = fopen( $logFile, 'a' );
 
-//echo "123456";
-
-$fp = fopen( "testfile", "a" );
-fwrite( $fp, "\n===================\n" );
-fwrite( $fp, "$text_string" );
+$text_string = 'default_value';
+//if( isset( $_SERVER['QUERY_STRING'] ) )
+//if( isset( $HTTP_RAW_POST_DATA ) )
+//{
+	$text_string = file_get_contents("php://input");
+//	$text_string = $HTTP_RAW_POST_DATA;
+//	$text_string = urldecode($_SERVER['QUERY_STRING']);
 fwrite( $fp, "\n------------------\n" );
+fwrite( $fp, $text_string );
+fwrite( $fp, "\n------------------\n" );
+//}
+//else
+//{
+//fwrite( $fp, "Expected variable was not set!" );
+//echo( $_SERVER['QUERY_STRING'] );
+//goto broken;
+//}
+
+/**
+  * Capture the raw input and put into an XML structure.
+  *
+  * Here are two examples of input.  The first is the trivial case and the second is more complex.
+	*  Both are REAL data...
+	*
+	* <ted5000 GWID="111111" auth="123456789">
+	* <MTU ID="104F7C" type="0">
+	* <cumulative timestamp="1346200560" watts="15640652" rate="0.06617"/>
+	* </MTU>
+	* </ted5000>
+	*
+	*
+	* <ted5000 GWID="111111" auth="123456789">
+	* <MTU ID="104F7C" type="0">
+	* <cumulative timestamp="1346200620" watts="15640696" rate="0.06617"/>
+	* <cumulative timestamp="1346200680" watts="15640739" rate="0.06617"/>
+	* <cumulative timestamp="1346200740" watts="15640783" rate="0.06617"/>
+	* <cumulative timestamp="1346200800" watts="15640827" rate="0.06617"/>
+	* <cumulative timestamp="1346200860" watts="15640871" rate="0.06617"/>
+	* <cumulative timestamp="1346200920" watts="15640915" rate="0.06617"/>
+	* <cumulative timestamp="1346200980" watts="15640959" rate="0.06617"/>
+	* <cumulative timestamp="1346201040" watts="15641003" rate="0.06617"/>
+	* <cumulative timestamp="1346201100" watts="15641047" rate="0.06617"/>
+	* <cumulative timestamp="1346201160" watts="15641091" rate="0.06617"/>
+	* <cumulative timestamp="1346201220" watts="15641135" rate="0.06617"/>
+	* <cumulative timestamp="1346201280" watts="15641179" rate="0.06617"/>
+	* <cumulative timestamp="1346201340" watts="15641223" rate="0.06617"/>
+	* </MTU>
+	* </ted5000>
+	*
+	*
+  */
+
 
 $xml = new SimpleXMLElement( $text_string );
 
-connect_to_db();
-global $link;
-$stmt = $link->stmt_init();
 
-if( $stmt->prepare( "INSERT INTO " . $table_prefix . "elec_usage ( date, watts, rate ) VALUES ( FROM_UNIXTIME(?), ?, ? )" ) )
+try
 {
-  foreach( $xml->children() as $child )
-  {
+	$sql = "INSERT INTO {$dbConfig['table_prefix']}elec_usage( date, watts, rate ) VALUES ( FROM_UNIXTIME(?), ?, ? )";
+	$queryRunInsert = $pdo->prepare( $sql );
+}
+catch( Exception $e )
+{
+	fwrite( $fp, "\nSQL PREP ERROR\n" );
+  doError( 'DB Exception: ' . $e->getMessage() );
+}
+
+
+foreach( $xml->children() as $child )
+{
     foreach( $child->children() as $subchild )
     {
       foreach( $subchild->attributes() as $attr2 )
       {
         switch( $attr2->getName() )
         {
-          case "timestamp":
+				case 'timestamp':
             //echo "<br>Found match for $attr2->getName() of $attr2";
             $timestamp = $attr2;
             //echo "<br>Found match for timestamp of $timestamp";
           break;
-          case "watts":
+				case 'watts':
             $watts = $attr2;
           break;
-          case "rate":
+				case 'rate':
             $rate = $attr2;
           break;
           //default:
@@ -44,24 +102,17 @@ if( $stmt->prepare( "INSERT INTO " . $table_prefix . "elec_usage ( date, watts, 
         }
         //echo '{{{'. $attr2->getName().', '.$attr2. '}}}';
       }
-$sql = "INSERT INTO " . $table_prefix . "elec_usage ( date, watts, rate ) VALUES ( FROM_UNIXTIME($timestamp), $watts, $rate )";
-fwrite( $fp, "SQL is $sql (THIS SQL IS NOT USED!!!!!\n)" );
+
+$sql = "INSERT INTO {$dbConfig['table_prefix']}elec_usage ( date, watts, rate ) VALUES ( FROM_UNIXTIME($timestamp), $watts, $rate )";
+fwrite( $fp, "SQL is $sql (THIS SQL IS NOT USED!!!!!)\n" );
 //$result = mysql_query( $sql );
 
-      $stmt->bind_param( "sss", $timestamp, $watts, $rate );
-      $stmt->execute();
+    $queryRunInsert->execute( array( $timestamp, $watts, $rate ) );
     }
-  }
-}
-else
-{
-  log_it( "No joy prepping SQL" );
 }
 
-
+broken:
 fwrite( $fp, "\n===================\n" );
 fclose( $fp );
 
-$stmt->close();
-disconnect_from_db();
 ?>
